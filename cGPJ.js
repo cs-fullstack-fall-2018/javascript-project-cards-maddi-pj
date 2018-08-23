e799a9a6e1701181562ed71de7738b4bca27796f
Pelane Sanon <p_sanon@aol.com>
2018-08-23 11:18:27 -0500

    Progress

--- cGPJ.js
+++ cGPJ.js
@@ -1,47 +1,16 @@
+// An if/else statement that declares a winner
+function winner()
+{
+    if (player1Card.cardNumber>player2Card.cardNumber)
+        {
+            console.log("Player 1 wins");
+            console.log("Player 1: ", player1Card, "Player 2: ", player2Card);
+                for (var score=0; )
+        }
+    else
+        {
+            console.log("Player 2 wins");
+            console.log("Player 2: ", player2Card, "Player 1: ", player1Card)
 
-if (player1Card.cardNumber>player2Card.cardNumber)
-    {
-        console.log("Player 1 wins");
-        console.log("Player 1: ", player1Card, "Player 2: ", player2Card);
-    }
-else
-    {
-        console.log("Player 2 wins");
-        console.log("Player 2: ", player2Card, "Player 1: ", player1Card)
-    }
-
-// function main() {
-//     let cards = new Array();
-//     let typeArray = ["s", "d", "c", "h"];
-//     let cardNumberArray = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"];
-//
-//     for(let i=0; i <typeArray.length; i++)
-//     {
-//         for(let x=0; x <cardNumberArray.length; x++)
-//         {
-//             let card = {cardNumber: cardNumberArray[x], type: typeArray[i]};
-//             cards.push(card);
-//         }
-//     }
-//
-//     for (var i=0; i < 10000; i++)
-//     {
-//         let rand1 = Math.floor((Math.random() * cards.length));
-//         let rand2 = Math.floor((Math.random() * cards.length));
-//         let tmp = cards[rand1];
-//         cards[rand1] = cards[rand2];
-//         cards[rand2] = tmp;
-//     }
-//
-//
-//     var player1Deck = [];
-//     var player2Deck = [];
-//
-// // The code to split the deck
-//     player1Deck = cards.slice();
-//     player2Deck = player1Deck.splice(1, 26);
-//
-//     console.log(player1Deck);
-//     console.log(player2Deck);
-// }
-
+        }
+}
